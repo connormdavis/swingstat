e8630ed0df99471c3e0b150e38325177afebe7d9
connormdavis <connor.m.davis.22@dartmouth.edu>
2022-02-14 13:24:36 -0500

    Initial Commit

---
+++ Shared/SwingStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwingStatApp.swift
+//  Shared
+//
+//  Created by Connor Davis on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwingStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
