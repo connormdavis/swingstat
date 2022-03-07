285ff10add40d006c9484c54219a53d44bcdc26a
chriscsykes <chrispsykes12@gmail.com>
2022-03-06 19:45:36 -0500

    Minor UI changes

--- SwingStat/Swing Analyzer/SwingTipDetailed.swift
+++ SwingStat/Swing Analyzer/SwingTipDetailed.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct SwingTipDetailed: View {
+    
     var body: some View {
         VStack{
             Text("[Swing Tip Title]")
@@ -16,6 +17,7 @@ struct SwingTipDetailed: View {
             Text("[Description]")
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .font(.body)
+                .padding(.top, 20)
         }
         .padding(.horizontal, 25)
         .padding(.top, 20)
@@ -25,14 +27,15 @@ struct SwingTipDetailed: View {
             Text("Helpful Resources:")
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .font(.title3)
+                .padding(.horizontal, 25)
+                .padding(.top, 20)
             List {
                 Text("https://www.youtube.com/watch?v=eyNSo4WrWlU")
                 Text("https://www.youtube.com/channel/UCfi-mPMOmche6WI-jkvnGXw")
                 Text("https://golfinsideruk.com/golf-backswing-explained/")
             }
         }
-        .padding(.horizontal, 25)
-        .padding(.top, 20)
+
     }
 }
 
