f66811f76d715076ced9d25ac7d399a507c8187e
Connor Davis <connor.m.davis.22@dartmouth.edu>
2022-05-11 13:59:50 -0400

    added prereqs for backend

---
+++ SwingStat/Model/SavedSwingAnalysis.swift
@@ -0,0 +1,8 @@
+//
+//  SavedSwingAnalysis.swift
+//  SwingStat (iOS)
+//
+//  Created by Connor Davis on 5/10/22.
+//
+
+import Foundation
---
+++ SwingStat/Model/UserData.swift
@@ -0,0 +1,8 @@
+//
+//  UserData.swift
+//  SwingStat (iOS)
+//
+//  Created by Connor Davis on 5/10/22.
+//
+
+import Foundation
